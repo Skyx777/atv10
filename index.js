da80275aee2bdaa9c7e9bff281f587f106edff1e
Skyx777 <113266792+Skyx777@users.noreply.github.com>
2022-12-17 22:02:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//////////1 parte////////////
+const e= document.querySelector("header");
+ e= document.createElement("h1")
+  //////////////////////////
+
+  ///////////2 parte/////////////
+ const u = document.querySelector("main");
+ 
+ u=document.createElement("div_1")
+ const m= document.querySelector("div_1");
+ m= document.createElement("h2")
+ m= document.createElement("p");
+/////////////////////////////
+u= document.createElement("div_2");
+const k= document.querySelector("div_2");
+k= document.createElement("h2");
+k= document.createElement("p")
